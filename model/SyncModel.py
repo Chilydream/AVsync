81dba72492a62754c5b2e8ca4916715e60372d4d
ChilyDream <chilydream@gmail.com>
2021-12-14 15:04:13 +0800

    说话还是沉默

--- model/SyncModel.py
+++ model/SyncModel.py
@@ -42,3 +42,39 @@ class SyncModel(nn.Module):
 		x = torch.cat((lip, voice), dim=1)
 		x = self.model(x)
 		return x
+
+
+class SilenceModel(nn.Module):
+	# 一分钟大约能说 160~180 个汉字
+	# 每个汉字平均发音时长约为 350ms
+	# 假设视频帧数是 25fps，则每个汉字大约对应 9帧
+	def __init__(self, lip_emb, seq_len=29):
+		super(SilenceModel, self).__init__()
+		self.lip_emb = lip_emb
+		self.seq_len = seq_len
+
+		# x = (b, seq, lip_emb+voice_emb)
+		self.model = nn.Sequential(
+			nn.Linear(lip_emb, 512),
+			nn.BatchNorm1d(512),
+			nn.ReLU(True),
+			nn.Linear(512, 256),
+			nn.BatchNorm1d(256),
+			nn.ReLU(True),
+			nn.Linear(256, 128),
+			nn.BatchNorm1d(128),
+			nn.ReLU(True),
+			nn.Linear(128, 64),
+			nn.BatchNorm1d(64),
+			nn.ReLU(True),
+			nn.Linear(64, 32),
+			nn.BatchNorm1d(32),
+			nn.ReLU(True),
+			nn.Linear(32, 2),
+			nn.Softmax(-1),
+		)
+
+	def forward(self, lip):
+		# x.shpae = (batch, lip_emb)
+		x = self.model(lip)
+		return x
--- silence_judge.py
+++ silence_judge.py
@@ -8,8 +8,9 @@ import torchvision.transforms as transforms
 import wandb
 import sys
 
-from model.SyncModel import SyncModel
+from model.SyncModel import SyncModel, SilenceModel
 from utils.accuracy import get_gt_label, get_rand_idx
+from utils.data_utils.LabLmk import LabLmkDataLoader
 from utils.data_utils.LabLmkWav import LabLmkWavDataLoader
 from utils.data_utils.LabRaw import LabDataLoader
 
@@ -26,7 +27,7 @@ from third_party.yolo.yolo_utils.util_yolo import face_detect
 from third_party.HRNet.utils_inference import get_model_by_name, get_batch_lmks
 
 
-def evaluate(model_lmk2lip, model_wav2v, model_sync, criterion_class, loader, args):
+def evaluate(model_lmk2lip, model_sync, criterion_class, loader, args):
 	run_device = torch.device("cuda:0" if args.gpu else "cpu")
 
 	val_loss_gt = Meter('Match Class Loss', 'avg', ':.4f')
@@ -85,7 +86,7 @@ def evaluate(model_lmk2lip, model_wav2v, model_sync, criterion_class, loader, ar
 
 def main():
 	# ===========================参数设定===============================
-	args = TrainOptions('config/lab_sync.yaml').parse()
+	args = TrainOptions('config/lab_silence.yaml').parse()
 	start_epoch = 0
 	batch_size = args.batch_size
 	batch_first = args.batch_first
@@ -105,17 +106,10 @@ def main():
 
 	# ============================模型载入===============================
 	print(f'{"="*20}Start loading model{"="*20}')
-	model_yolo = yolo_model(cfg='config/yolov5s.yaml').float().fuse().eval()
-	model_yolo.load_state_dict(torch.load('pretrain_model/raw_yolov5s.pt'))
-	model_yolo.to(run_device)
-	model_hrnet = get_model_by_name('300W', root_models_path='pretrain_model')
-	model_hrnet = model_hrnet.eval()
-	model_hrnet.to(run_device)
 
 	model_lmk2lip = Lmk2LipModel(lmk_emb=args.lmk_emb, lip_emb=args.lip_emb, stride=1)
-	model_wav2v = VGGVoice(n_out=args.voice_emb)
-	model_sync = SyncModel(lip_emb=args.lip_emb, voice_emb=args.voice_emb)
-	model_list = [model_lmk2lip, model_wav2v, model_sync]
+	model_sync = SilenceModel(lip_emb=args.lip_emb)
+	model_list = [model_lmk2lip, model_sync]
 	for model_iter in model_list:
 		model_iter.to(run_device)
 		model_iter.train()
@@ -124,30 +118,25 @@ def main():
 	                                 transforms.Resize(args.face_resolution)])
 
 	optim_lmk2lip = optim.Adam(model_lmk2lip.parameters(), lr=args.lmk2lip_lr, betas=(0.9, 0.999))
-	optim_wav2v = optim.Adam(model_wav2v.parameters(), lr=args.wav2v_lr, betas=(0.9, 0.999))
 	optim_sync = optim.Adam(model_sync.parameters(), lr=args.sync_lr, betas=(0.9, 0.999))
 	criterion_class = nn.CrossEntropyLoss()
 	sch_lmk2lip = optim.lr_scheduler.ExponentialLR(optim_lmk2lip, gamma=args.lmk2lip_gamma)
-	sch_wav2v = optim.lr_scheduler.ExponentialLR(optim_wav2v, gamma=args.wav2v_gamma)
 	sch_sync = optim.lr_scheduler.ExponentialLR(optim_sync, gamma=args.sync_gamma)
-	tosave_list = ['model_lmk2lip', 'model_wav2v', 'model_sync',
-	               'optim_lmk2lip', 'optim_wav2v', 'optim_sync',
-	               'sch_lmk2lip', 'sch_wav2v', 'sch_sync']
+	tosave_list = ['model_lmk2lip', 'model_sync',
+	               'optim_lmk2lip', 'optim_sync',
+	               'sch_lmk2lip', 'sch_sync']
 	if args.wandb:
 		for model_iter in model_list:
 			wandb.watch(model_iter)
 
 	# ============================度量载入===============================
-	epoch_loss_class_gt = Meter('Match Class Loss', 'avg', ':.4f')
-	epoch_loss_class_fk = Meter('Mismatch Class Loss', 'avg', ':.4f')
+	epoch_loss_class = Meter('Match Class Loss', 'avg', ':.4f')
 	epoch_loss_final = Meter('Final Loss', 'avg', ':.4f')
-	epoch_acc_gt = Meter('Match Class ACC', 'avg', ':.2f', '%, ')
-	epoch_acc_fk = Meter('Mismatch Class ACC', 'avg', ':.2f', '%, ')
-	epoch_acc_all = Meter('All Class ACC', 'avg', ':.2f', '%, ')
+	epoch_acc = Meter('Match Class ACC', 'avg', ':.2f', '%, ')
 	epoch_timer = Meter('Time', 'time', ':4.0f')
 
-	epoch_reset_list = [epoch_loss_final, epoch_loss_class_gt, epoch_loss_class_fk,
-	                    epoch_acc_gt, epoch_acc_fk, epoch_acc_all,
+	epoch_reset_list = [epoch_loss_final, epoch_loss_class,
+	                    epoch_acc,
 	                    epoch_timer, ]
 	if args.mode in ('train', 'continue'):
 		print('Train Parameters')
@@ -159,15 +148,15 @@ def main():
 	loader_timer = Meter('Time', 'time', ':3.0f', end='')
 	print('%sStart loading dataset%s'%('='*20, '='*20))
 	loader_timer.set_start_time(time.time())
-	train_loader = LabLmkWavDataLoader(args.train_list, batch_size,
-	                                   num_workers=args.num_workers,
-	                                   seq_len=args.seq_len,
-	                                   is_train=True, max_size=0)
+	train_loader = LabLmkDataLoader(args.train_list, batch_size,
+	                                num_workers=args.num_workers,
+	                                seq_len=args.seq_len,
+	                                is_train=True, max_size=0)
 
-	valid_loader = LabLmkWavDataLoader(args.val_list, batch_size,
-	                                   num_workers=args.num_workers,
-	                                   seq_len=args.seq_len,
-	                                   is_train=True, max_size=0)
+	valid_loader = LabLmkDataLoader(args.val_list, batch_size,
+	                                num_workers=args.num_workers,
+	                                seq_len=args.seq_len,
+	                                is_train=True, max_size=0)
 	loader_timer.update(time.time())
 	print(f'Batch Num in Train Loader: {len(train_loader)}')
 	print(f'Finish loading dataset {loader_timer}')
@@ -184,24 +173,22 @@ def main():
 		if args.mode.lower() in ['valid', 'val', 'eval', 'evaluate']:
 			with torch.no_grad():
 				model_lmk2lip.eval()
-				model_wav2v.eval()
 				model_sync.eval()
 				criterion_class.eval()
-				evaluate(model_lmk2lip, model_wav2v, model_sync,
+				evaluate(model_lmk2lip, model_sync,
 				         criterion_class,
 				         valid_loader, args)
 		else:
 			del valid_loader
-			test_loader = LabLmkWavDataLoader(args.test_list, batch_size,
-			                                  num_workers=args.num_workers,
-			                                  seq_len=args.seq_len,
-			                                  is_train=True, max_size=0)
+			test_loader = LabLmkDataLoader(args.test_list, batch_size,
+			                               num_workers=args.num_workers,
+			                               seq_len=args.seq_len,
+			                               is_train=True, max_size=0)
 			with torch.no_grad():
 				model_lmk2lip.eval()
-				model_wav2v.eval()
 				model_sync.eval()
 				criterion_class.eval()
-				evaluate(model_lmk2lip, model_wav2v, model_sync,
+				evaluate(model_lmk2lip, model_sync,
 				         criterion_class,
 				         test_loader, args)
 		print(f'\n\nFinish Evaluation\n\n')
@@ -219,7 +206,6 @@ def main():
 		if args.pretrain_model is not None and os.path.exists(args.pretrain_model):
 			model_ckpt = torch.load(args.pretrain_model)
 			model_lmk2lip.load_state_dict(model_ckpt['model_lmk2lip'])
-			model_wav2v.load_state_dict(model_ckpt['model_wav2v'])
 			if 'model_sync' in model_ckpt.keys():
 				model_sync.load_state_dict(model_ckpt['model_sync'])
 	else:
@@ -241,65 +227,49 @@ def main():
 		batch_cnt = 0
 		epoch_timer.set_start_time(time.time())
 		for data in train_loader:
-			a_lmk, a_wav_gt, a_wav_fk = data
+			a_lmk, label_gt = data
 			a_lmk = a_lmk.to(run_device)
-			a_wav_gt = a_wav_gt.to(run_device)
-			a_wav_fk = a_wav_fk.to(run_device)
 			# a_lmk = (b, seq_len, 40)
 			# a_wav = (b, seq_len*16000)
 
 			a_lip = model_lmk2lip(a_lmk)
-			a_voice_gt = model_wav2v(a_wav_gt)
-			a_voice_fk = model_wav2v(a_wav_fk)
 
-			label_pred_gt = model_sync(a_lip, a_voice_gt)
-			label_pred_fk = model_sync(a_lip, a_voice_fk)
-			# print(f'\npred gt \n{label_pred_gt}\n')
-			# print(f'\npred fk \n{label_pred_fk}\n')
+			label_pred = model_sync(a_lip)
 
 			# ======================计算唇部特征单词分类损失===========================
-			loss_class_gt = criterion_class(label_pred_gt, label_one)
-			loss_class_fk = criterion_class(label_pred_fk, label_zero)
-			correct_num_gt = torch.sum(torch.argmax(label_pred_gt, dim=1) == label_one).item()
-			correct_num_fk = torch.sum(torch.argmax(label_pred_fk, dim=1) == label_zero).item()
+			loss_class = criterion_class(label_pred, label_gt)
+			correct_num = torch.sum(torch.argmax(label_pred, dim=1) == label_gt).item()
 
 			# ==========================反向传播===============================
 			optim_lmk2lip.zero_grad()
-			optim_wav2v.zero_grad()
 			optim_sync.zero_grad()
-			loss_final = loss_class_gt+loss_class_fk
+			loss_final = loss_class
 			loss_final.backward()
 			optim_lmk2lip.step()
-			optim_wav2v.step()
 			optim_sync.step()
 
 			# ==========计量更新============================
-			epoch_acc_gt.update(correct_num_gt*100/len(label_pred_gt))
-			epoch_acc_fk.update(correct_num_fk*100/len(label_pred_fk))
-			epoch_acc_all.update((epoch_acc_gt.avg+epoch_acc_fk.avg)*0.5)
-			epoch_loss_class_gt.update(loss_class_gt.item())
-			epoch_loss_class_fk.update(loss_class_fk.item())
+			epoch_acc.update(correct_num*100/len(label_gt))
+			epoch_loss_class.update(loss_class.item())
 			epoch_loss_final.update(loss_final.item())
 			epoch_timer.update(time.time())
 			batch_cnt += 1
 			print(f'\rBatch:{batch_cnt:04d}/{len(train_loader):04d}  {epoch_timer}{epoch_loss_final}',
-			      f'{epoch_acc_all}{epoch_acc_gt}{epoch_acc_fk}',
+			      f'{epoch_acc}',
 			      sep='', end='     ')
 			torch.cuda.empty_cache()
 
 		sch_lmk2lip.step()
-		sch_wav2v.step()
 		sch_sync.step()
 		print('')
 		print(f'Current Model M2V Learning Rate is {sch_lmk2lip.get_last_lr()}')
-		print(f'Current Model V2T Learning Rate is {sch_wav2v.get_last_lr()}')
 		print(f'Current Model Sync Learning Rate is {sch_sync.get_last_lr()}')
-		print('Epoch:', epoch, epoch_loss_final, epoch_acc_gt,
+		print('Epoch:', epoch, epoch_loss_final, epoch_acc,
 		      file=file_train_log)
 		log_dict = {'epoch': epoch,
 		            epoch_loss_final.name: epoch_loss_final.avg,
-		            epoch_loss_class_gt.name: epoch_loss_class_gt.avg,
-		            epoch_acc_gt.name: epoch_acc_gt.avg}
+		            epoch_loss_class.name: epoch_loss_class.avg,
+		            epoch_acc.name: epoch_acc.avg}
 		for meter in epoch_reset_list:
 			meter.reset()
 
@@ -320,16 +290,14 @@ def main():
 			with torch.no_grad():
 				# torch.no_grad()不能停止drop_out和 batch_norm，所以还是需要eval
 				model_lmk2lip.eval()
-				model_wav2v.eval()
 				model_sync.eval()
 				criterion_class.eval()
 				# try:
-				log_dict.update(evaluate(model_lmk2lip, model_wav2v, model_sync,
+				log_dict.update(evaluate(model_lmk2lip, model_sync,
 				                         criterion_class, valid_loader, args))
 				# except:
 				# 	print('Evaluating Error')
 				model_lmk2lip.train()
-				model_wav2v.train()
 				model_sync.train()
 				criterion_class.train()
 
