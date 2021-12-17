fc03fba21ae9e9fba40a9933bd539aaa80dca7e0
Chilydream <Chilydream@gmail.com>
2021-12-17 20:48:14 +0800

    删除batch_first

--- s2t_triplet.py
+++ s2t_triplet.py
@@ -78,7 +78,6 @@ def main():
 	args = TrainOptions('config/speech2text.yaml').parse()
 	start_epoch = 0
 	batch_size = args.batch_size
-	batch_first = args.batch_first
 	torch.backends.cudnn.benchmark = args.gpu
 	run_device = torch.device("cuda:0" if args.gpu else "cpu")
 
