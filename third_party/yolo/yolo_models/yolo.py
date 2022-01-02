734244db21ecb5e2c0cb5bd711b10666d5abbacb
Chilydream <Chilydream@gmail.com>
2022-01-02 23:46:16 +0800

    yolo.py 已删除debug代码，无多余输出

--- third_party/yolo/yolo_models/yolo.py
+++ third_party/yolo/yolo_models/yolo.py
@@ -164,9 +164,6 @@ class Model(nn.Module):
 				dt.append((time_synchronized()-t)*100)
 				print('%10.1f%10.0f%10.1fms %-40s'%(o, m.np, dt[-1], m.type))
 
-			print(len(x))
-			print(x[0].shape)
-			# todo: 删掉 print语句
 			x = m(x)  # run
 			y.append(x if m.i in self.save else None)  # save output
 
