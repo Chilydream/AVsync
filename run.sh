98135c39746ff771f4ed13e51f5ebe4d9e3458ba
chilydream <chilydream@gmail.com>
2022-03-11 21:08:28 +0800

    新机器上wav2lip的syncnet模块

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --comment=cross_modal
 
 ### 给你这个作业起个名字，方便识别不同的作业
-#SBATCH --job-name=sync_multi
+#SBATCH --job-name=wav2lip_syncnet
 
 ### 指定该作业需要多少个节点
 ### 注意！没有使用多机并行（MPI/NCCL等），下面参数写1！不要多写，多写了也不会加速程序！
@@ -12,11 +12,15 @@
 
 ### 指定该作业需要多少个CPU核心
 ### 注意！一般根据队列的CPU核心数填写，比如cpu队列64核，这里申请64核，并在你的程序中尽量使用多线程充分利用64核资源！
-#SBATCH --ntasks=2
+#SBATCH --ntasks=16
 
 ### 指定该作业在哪个队列上执行
 ### 目前可用的队列有 cpu/fat/titan/tesla
 #SBATCH --partition=titan
+
+### 申请一块GPU卡，一块GPU卡默认配置了一定数量的CPU核
+### 注意！程序没有使用多卡并行优化的，下面参数写1！不要多写，多写也不会加速程序！
+#SBATCH --gpus=1
 
 ### 以上参数用来申请所需资源
 ### 以下命令将在计算节点执行
@@ -26,6 +30,8 @@ export PATH=/opt/app/anaconda3/bin:$PATH
 ### 激活一个 Anaconda 环境 tf22
 source activate torch110
 
+nvidia-smi
 ### 执行你的作业
-### salloc --nodes=1 --partition=tesla --time=00:30:00 --comment=cross_modal
-python GetMetaData.py
+### salloc --nodes=1 --partition=titan --time=10:00:00 --comment=cross_modal --gpus=1
+### salloc --nodes=1 --ntasks=2 --partition=cpu --time=10:00:00 --comment=cross_modal
+python sync_wav2lip.py
